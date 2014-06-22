514824d20c081e74c9e4fcc0fc3089ea5755f460
Babken Vardanyan <483ken@gmail.com>
2014-06-22 16:25:21 +0400

    log formatting

--- logger.py
+++ logger.py
@@ -7,7 +7,7 @@ logger.setLevel(logging.DEBUG)
 log_to_file = logging.FileHandler('log')
 log_to_file.setLevel(logging.DEBUG)
 log_to_file.setFormatter(logging.Formatter('%(asctime)s '
-                                           '%(filename)s '
+                                           '%(filename)s:'
                                            '%(lineno)4d '
                                            '%(levelname)s:'
                                            '%(message)s  '
@@ -16,7 +16,7 @@ logger.addHandler(log_to_file)
 
 log_to_console = logging.StreamHandler()
 log_to_console.setLevel(logging.INFO)
-log_to_console.setFormatter(logging.Formatter('%(filename)s '
+log_to_console.setFormatter(logging.Formatter('%(filename)s:'
                                               '%(lineno)4d '
                                               '%(levelname)s:'
                                               '%(message)s  '
