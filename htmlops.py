d408b45e0e93cbd267449d4ed420541462841ebc
Babken Vardanyan <483ken@gmail.com>
2014-06-25 20:52:28 +0400

    added docstrings to htmlops

--- htmlops.py
+++ htmlops.py
@@ -1,3 +1,8 @@
+''' htmlops.py - HTML operations.
+
+    This only functions called from outside are in the HtmlRequests class.
+    They return 
+'''
 # Standard modules
 import logging
 
@@ -10,7 +15,11 @@ log.addHandler(log_handlers.TO_FILE)
 log.setLevel(logging.INFO)
 
 class HtmlTags:
-    ''' Single HTML tags formatted for opening or closing. '''
+    ''' Single HTML tags formatted for opening or closing.
+    
+        Data is accessed from HtmlRequests using the functions
+        opening() or closing().
+    '''
     paragraph = 'p'
     bold = 'b'
     italic = 'i'
@@ -23,78 +32,100 @@ class HtmlTags:
     definition_data = 'dd'
 
     def opening(tag):
+        ''' Returns a single opening tag. '''
         return '<' + tag + '>'
 
     def closing(tag):
+        ''' Returns a single closing tag. '''
         return '</' + tag + '>'
 
 
 class HtmlRequests:
-    ''' Returns strings composed of one or more tags. '''
-    def open_paragraph():
-        result = HtmlTags.opening(HtmlTags.paragraph)
-        log.debug(result)
-        return result
-    def close_paragraph():
-        result = HtmlTags.closing(HtmlTags.paragraph)
-        log.debug(result)
-        return result
-
-    def open_pre():
-        result = HtmlTags.opening(HtmlTags.pre)
-        log.debug(result)
-        return result
-    def close_pre():
-        result = HtmlTags.closing(HtmlTags.pre)
-        log.debug(result)
-        return result
-
-    def open_code():
-        result = HtmlTags.opening(HtmlTags.code)
-        log.debug(result)
-        return result
-    def close_code():
-        result = HtmlTags.closing(HtmlTags.code)
-        log.debug(result)
-        return result
+    ''' Returns strings composed of a pair of opening and closing tags.
+    
+        Exceptions include the document header and footer which are
+        rather complex.
+    '''
 
     def open_italic():
+        ''' Returns '<i>' '''
         result = HtmlTags.opening(HtmlTags.italic)
         log.debug(result)
         return result
     def close_italic():
+        ''' Returns '</i>' '''
         result = HtmlTags.closing(HtmlTags.italic)
         log.debug(result)
         return result
 
     def open_bold():
+        ''' Returns '<b>' '''
         result = HtmlTags.opening(HtmlTags.bold)
         log.debug(result)
         return result
     def close_bold():
+        ''' Returns '</b>' '''
         result = HtmlTags.closing(HtmlTags.bold)
         log.debug(result)
         return result
 
     def open_code_italic():
+        ''' Returns '<code><i>' '''
         result = HtmlRequests.open_code() + HtmlRequests.open_italic()
         log.debug(result)
         return result
     def close_italic_code():
+        ''' Returns '</i></code>' '''
         result = HtmlRequests.close_italic() + HtmlRequests.close_code()
         log.debug(result)
         return result
 
     def open_code_bold():
+        ''' Returns '<code><b>' '''
         result = HtmlRequests.open_code() + HtmlRequests.open_bold()
         log.debug(result)
         return result
     def close_bold_code():
+        ''' Returns '</b></code>' '''
         result = HtmlRequests.close_bold() + HtmlRequests.close_code()
         log.debug(result)
         return result
 
+    def open_paragraph():
+        ''' Returns '<p>' '''
+        result = HtmlTags.opening(HtmlTags.paragraph)
+        log.debug(result)
+        return result
+    def close_paragraph():
+        ''' Returns '</p>' '''
+        result = HtmlTags.closing(HtmlTags.paragraph)
+        log.debug(result)
+        return result
+
+    def open_pre():
+        ''' Returns '<pre>' '''
+        result = HtmlTags.opening(HtmlTags.pre)
+        log.debug(result)
+        return result
+    def close_pre():
+        ''' Returns '</pre>' '''
+        result = HtmlTags.closing(HtmlTags.pre)
+        log.debug(result)
+        return result
+
+    def open_code():
+        ''' Returns '<code>' '''
+        result = HtmlTags.opening(HtmlTags.code)
+        log.debug(result)
+        return result
+    def close_code():
+        ''' Returns '</code>' '''
+        result = HtmlTags.closing(HtmlTags.code)
+        log.debug(result)
+        return result
+
     def section_title(title):
+        ''' Returns '<h2>TITLE</h2>' '''
         result = HtmlTags.opening(HtmlTags.header_level_2) + \
                  title + \
                  HtmlTags.closing(HtmlTags.header_level_2) + \
@@ -103,6 +134,7 @@ class HtmlRequests:
         return result
 
     def subsection_title(title):
+        ''' Returns '<h3>TITLE</h3' '''
         result = HtmlTags.opening(HtmlTags.header_level_3) + \
                  title + \
                  HtmlTags.closing(HtmlTags.header_level_3) + \
@@ -110,7 +142,49 @@ class HtmlRequests:
         log.debug(result)
         return result
 
+    def open_definition_list():
+        ''' Returns '<dl>' '''
+        result = HtmlTags.opening(HtmlTags.definition_list)
+        log.debug(result)
+        return result
+    def close_definition_list():
+        ''' Returns '</dl>' '''
+        result = HtmlTags.closing(HtmlTags.definition_list)
+        log.debug(result)
+        return result
+
+    def open_definition_name():
+        ''' Returns '<dt>' '''
+        result = HtmlTags.opening(HtmlTags.definition_name)
+        log.debug(result)
+        return result
+    def close_definition_name():
+        ''' Returns '</dt>' '''
+        result = HtmlTags.closing(HtmlTags.definition_name)
+        log.debug(result)
+        return result
+
+    def open_definition_data():
+        ''' Returns '<dd>' '''
+        result = HtmlTags.opening(HtmlTags.definition_data)
+        log.debug(result)
+        return result
+    def close_definition_data():
+        ''' Returns '</dd>' '''
+        result = HtmlTags.closing(HtmlTags.definition_data)
+        log.debug(result)
+        return result
+
     def document_header(name, man_section):
+        ''' Returns the HTML header, which includes:
+            
+            1. The doctype declaration
+            2. Stuff in <head> tag, including:
+                a. Charset declaration
+                b. Title, composed of manpage name and it's section name
+                c. CSS stylesheet name
+            3. The document title at the top enclosed in level 1 header tags
+        '''
         header = '<!doctype HTML>\n' \
                  '<html>\n' \
                  '<head>\n' \
@@ -124,35 +198,11 @@ class HtmlRequests:
         log.debug(result)
         return result
     def document_footer():
+        ''' Returns the HTML footer, which closes what has been left open by
+            document_header(): the closing 'body' and 'html' tags
+        '''
         footer = '</body>\n' \
                  '</html>\n'
         log.debug(footer)
         return footer
-
-    def open_definition_list():
-        result = HtmlTags.opening(HtmlTags.definition_list)
-        log.debug(result)
-        return result
-    def close_definition_list():
-        result = HtmlTags.closing(HtmlTags.definition_list)
-        log.debug(result)
-        return result
-
-    def open_definition_name():
-        result = HtmlTags.opening(HtmlTags.definition_name)
-        log.debug(result)
-        return result
-    def close_definition_name():
-        result = HtmlTags.closing(HtmlTags.definition_name)
-        log.debug(result)
-        return result
-
-    def open_definition_data():
-        result = HtmlTags.opening(HtmlTags.definition_data)
-        log.debug(result)
-        return result
-    def close_definition_data():
-        result = HtmlTags.closing(HtmlTags.definition_data)
-        log.debug(result)
-        return result
 
